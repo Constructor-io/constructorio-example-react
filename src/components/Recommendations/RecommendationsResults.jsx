ceabf2ffeba36ee20a820c5c96e549af9990a059
Steve Blaurock <sblaurock@gmail.com>
2022-11-13 22:20:49 -0700

    Disable carousel scrolling.

--- src/components/Recommendations/RecommendationsResults.jsx
+++ src/components/Recommendations/RecommendationsResults.jsx
@@ -10,21 +10,21 @@ const responsive = {
       max: 3000,
       min: 1024,
     },
-    items: 3,
+    items: 4,
     partialVisibilityGutter: 40,
-  },
-  mobile: {
-    breakpoint: {
-      max: 464,
-      min: 0,
-    },
-    items: 1,
-    partialVisibilityGutter: 30,
   },
   tablet: {
     breakpoint: {
       max: 1024,
-      min: 464,
+      min: 768,
+    },
+    items: 3,
+    partialVisibilityGutter: 30,
+  },
+  mobile: {
+    breakpoint: {
+      max: 768,
+      min: 500,
     },
     items: 2,
     partialVisibilityGutter: 30,
@@ -51,8 +51,9 @@ function RecommendationsResults(props) {
         containerClass="container"
         itemClass=""
         sliderClass=""
-        slidesToSlide={ 1 }
+        slidesToSlide={ 0 }
         centerMode
+        removeArrowOnDeviceType={ ['tablet', 'mobile', 'desktop'] }
         draggable={ false }
         { ...dataAttributes }
       >
